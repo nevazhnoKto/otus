a79f4f924d06de226e369b3dece31dbc525dcedb
Иван Тамакулов <itamakulov@gmail.com>
2024-01-12 12:44:41 +0400

    1 ДЗ .

---
+++ Spacebattle/Spacebattle/FuncGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spacebattle
+{
+    public static class FuncGenerator<T>
+    {
+        public static Func<T, T, T> CreateOperatorFuncAdd(Func<ParameterExpression, ParameterExpression, BinaryExpression> f)
+        {
+            ParameterExpression ap = Expression.Parameter(typeof(T), "a");
+            ParameterExpression bp = Expression.Parameter(typeof(T), "b");
+            Expression operationResult = f(ap, bp);
+
+            Expression<Func<T, T, T>> lambda = Expression.Lambda<Func<T, T, T>>(operationResult, ap, bp);
+            return lambda.Compile();
+        }
+
+        public static Func<T[], T[], T[]> CreateArrayOperatorFuncAdd(Func<ParameterExpression, ParameterExpression, BinaryExpression> f)
+        {
+            Func<T, T, T> op = CreateOperatorFuncAdd(f);
+            return (a, b) =>
+            {
+                int len = a.Length;
+                T[] result = new T[len];
+                for (int i = 0; i < len; i++)
+                    result[i] = op(a[i], b[i]);
+                return result;
+            };
+        }
+    }
+}
--- Spacebattle/Spacebattle/Vector.cs
+++ Spacebattle/Spacebattle/Vector.cs
@@ -9,57 +9,76 @@ namespace Spacebattle
 {
     public class Vector<T> : IEnumerable<T>
     {
+        private int _size = 0;
         private T[] _values;
 
         public Vector(IEnumerable<T> initialValues)
         {
-            // Задача 1.
+            if (initialValues == null || !initialValues.Any())
+                throw new ArgumentException("Cписок NULL или пуст", nameof(initialValues));
+            _size = initialValues.Count();
+            _values = initialValues.ToArray();
+        }
+
+        public T[] Values
+        {
+            get
+            {
+                return _values;
+            }
+            set
+            {
+                _values = value;
+            }
         }
 
         public int Size
         {
             get
             {
-                //Задача 2
-                return 0; // Это заглушка, чтобы компилировался код
+                return _size;
+            }
+            set
+            {
+                _size = value;
             }
         }
 
         public static implicit operator Vector<T>(T[] a)
         {
-            //Задача 3
-            return null; // Это заглушка, чтобы компилировался код
+            return new Vector<T>(a);
         }
 
         IEnumerator<T> IEnumerable<T>.GetEnumerator()
         {
-            //Задача 4
-            return null; // Это заглушка, чтобы компилировался код
+            return _values.AsEnumerable().GetEnumerator();
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            //Задача 4
-            return null; // Это заглушка, чтобы компилировался код
-        }
+        IEnumerator IEnumerable.GetEnumerator() => _values.GetEnumerator();
 
         public override string ToString()
         {
-            //Задача 5
-            return string.Empty; // Это заглушка, чтобы компилировался код
+            return $"({string.Join(", ", _values)})";
         }
 
         public static T[] Parse(string value, Func<string, T> parse)
         {
-            //Задача 6
-            return new T[0]; // Это заглушка, чтобы компилировался код
+            if (value.FirstOrDefault() != '(' | value.LastOrDefault() != ')')
+                throw new FormatException(string.Format("{0}  is not valid format for type {1}", value, typeof(T[]).ToString()));
+
+            string tmpStr = value.Substring(1, value.Length - 2).Trim();
+            string[] items = tmpStr.Split(',');
+            var values = items.Select(s => parse(s.Trim()));
+            return values.ToArray();
         }
 
         public static Vector<T> operator +(Vector<T> a, Vector<T> b)
         {
-            //Задача 7
-            return null; // Это заглушка, чтобы компилировался код
+            if (a.Size != b.Size)
+                throw new ArgumentException("Невозможно сложить массивы разной величны!");
+            return AddT(a.Values.ToArray(), b.Values.ToArray());
         }
 
+        private static Func<T[], T[], T[]> AddT => FuncGenerator<T>.CreateArrayOperatorFuncAdd((a, b) => Expression.Add(a, b));
     }
 }
