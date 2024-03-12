a2388c1b4f83d01650feeeeba1ade8505105bf44
Тамакулов Иван Сергеевич <ivan.tamakulov@russianpost.ru>
2024-03-12 17:11:46 +0400

    ДЗ №4

---
+++ Spacebattle/ConsoleApp2/PluginInstanceClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class PluginInstanceClass
+    {
+        public object Plugin { get; set; }
+
+        public string FullName { get; set; }
+        public bool IsLoad { get; set; }
+
+        public int RetryCount { get; set; }
+
+        public PluginInstanceClass(object plug, string fullName)
+        {
+            Plugin = plug;
+            FullName = fullName;
+        }
+
+    }
+}
--- Spacebattle/ConsoleApp2/Program.cs
+++ Spacebattle/ConsoleApp2/Program.cs
@@ -1,32 +1,66 @@
 ﻿using System.Diagnostics;
 using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Collections.Concurrent;
 
 namespace ConsoleApp2
 {
-    internal static class Program
+    internal class Program
     {
+        static private object threadLock = new object();
+        static public List<PluginInstanceClass> _listPlugins = new List<PluginInstanceClass>();
         static void Main(string[] args)
         {
-            ConcurrentList<object> concurrentList = new ConcurrentList<Object>()
-            var path = @"C:\work\OTUS_new\Spacebattle\ConsoleApp3\bin\Debug\net8.0\ConsoleApp3.dll";
-            var assembly = Assembly.LoadFrom(path);
+            var path = @"C:\test.dll";
+            Task.Run(() => LoadDLL(path));
+            Task.Run(() => RunLoad());
+        }
+
+        static private void LoadDLL(string pathDLL)
+        {
+            var assembly = Assembly.LoadFrom(pathDLL);
             var types = assembly.GetTypes();
-            foreach(var type in types)
+            foreach (var type in types)
             {
                 if (type.GetInterfaces().Any(i => i.Name == "IPlugin"))
                 {
                     var instance = Activator.CreateInstance(type);
+                    AddInQueue(instance, type.FullName);
+                }
+            }
+            return;
+        }
+        static private void RunLoad()
+        {
+            while (_listPlugins.Any(p => !p.IsLoad && p.RetryCount > 2))
+            {
+                lock (threadLock)
+                {
+                    foreach (var plug in _listPlugins)
+                    {
+                        try
+                        {
+                            var load = plug.GetType().GetMethod("Load");
+                            load.Invoke(plug, Array.Empty<object>());
+                            plug.IsLoad = true;
+                        }
+                        catch
+                        {
+                            plug.RetryCount++;
+                        }
+                    }
 
-                    AddInSequesnce();
-                    var load = type.GetMethod("Load");
-                    load.Invoke(instance, Array.Empty<object>());
-
-                    
                 }
             }
         }
 
+        static private void AddInQueue(object instance, string fullName)
+        {
+            if (_listPlugins.Any(p => p.FullName == fullName))
+                lock (threadLock)
+                {
+                    _listPlugins.Add(new PluginInstanceClass(instance, fullName));
+                }
+        }
+
     }
+
 }
