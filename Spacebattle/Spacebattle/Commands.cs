bd44bb6b5cddf0dd0f854521af82380958b85b40
Тамакулов Иван Сергеевич <ivan.tamakulov@russianpost.ru>
2024-05-01 13:11:16 +0400

    HW#6_TamakulovIvan

---
+++ Spacebattle/Spacebattle/Commands.cs
@@ -0,0 +1,49 @@
+﻿using Spacebattle.Interface;
+
+namespace Spacebattle
+{
+    class Command1 : ICommand
+    {
+        ICommand _next;
+        public Command1(ICommand next) => _next = next;
+        public async Task ExecuteAsync()
+        {
+            await _next.ExecuteAsync();
+            Console.WriteLine("Команда1");
+        }
+    }
+
+    class Command2 : ICommand
+    {
+        ICommand _next;
+        public Command2(ICommand next) => _next = next;
+
+        public async Task ExecuteAsync()
+        {
+            Console.WriteLine("Команда2");
+            if (_next != null)
+                await _next.ExecuteAsync();
+        }
+    }
+
+    class Command3 : ICommand
+    {
+        ICommand _next;
+        public Command3(ICommand next) => _next = next;
+
+        public async Task ExecuteAsync()
+        {
+            Console.WriteLine("Команда3");
+            if (_next != null)
+                await _next.ExecuteAsync();
+        }
+    }
+
+    class LastCommand : ICommand
+    {
+        public async Task ExecuteAsync()
+        {
+            await Task.Delay(500);
+        }
+    }
+}
---
+++ Spacebattle/Spacebattle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spacebattle
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var lastCommand = new LastCommand();
+            var command3 = new Command3(lastCommand);
+            var command2 = new Command2(command3);
+            var command1 = new Command1(command2);
+
+            await command1.ExecuteAsync();
+            Console.ReadKey();
+        }
+    }
+}
